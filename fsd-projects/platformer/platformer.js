471368b53b8395fa0afe5a89f7d5583c3564ea2b
LLEB01 <leblan526@wearescpps.org>
2025-02-07 14:46:06 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,33 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(500, 630, 300, 1000);
+    createPlatform(950, 500, 190, 400);
+    createPlatform(800, 400, 300, 10)
+    createPlatform(900, 0, 100, 300);
+    createPlatform(700, 300, 100, 10);
+    createPlatform(1245, 500, 200, 50);
+    createPlatform(1100, 630, 100, 20);
+    createPlatform(100, 0, 20, 400);
+    createPlatform(1200, 300, 200, 10);
+    createPlatform(1000, 200, 300, 10);
+    createPlatform(500, 200, 50, 10);
+    createPlatform(100, 300, 100, 10);
+   createPlatform(0, 650, 300, 150)
     // TODO 3 - Create Collectables
-
-
+    createCollectable("database" ,1250,700, 0, 0);
+    createCollectable("database", 1020, 150, 0, 0);
+    createCollectable("database", 130, 250, 0, 0);
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("bottom", 800, 500);
+    createCannon("top", 1200, 700)
+    createCannon("bottom", 300, 600)
 
     
     
